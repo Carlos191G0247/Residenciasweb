bf9a59011711c36d0f83954ea4eec04f6e1aa610
CARLOS <CARLOS@DESKTOP-4CE6L01>
2023-10-09 10:33:30 -0600

    Agregar archivos de proyecto.

---
+++ Residenciasweb/pages/login/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Residenciasweb.pages.login
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
