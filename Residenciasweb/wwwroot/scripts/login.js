0256db6118573fd7e59ce916accfdc4ae29b1dd3
carlos gallegos <gallegoscarlos616@gmail.com>
2023-10-10 11:52:55 -0600

    cambios

---
+++ Residenciasweb/wwwroot/scripts/login.js
@@ -0,0 +1,8 @@
+﻿var formRecuperar = document.querySelector(".formRecuperarContraseña");
+var formMensaje = document.querySelector(".formMensaje");
+
+document.body.addEventListener("click", function (e) {
+    if (e.target.tagName == "A") {
+        formRecuperar.parentElement.style.display = "block";
+    } 
+});
