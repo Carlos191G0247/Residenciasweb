e505ff9b7191259f85fa45ed036545a1fd0bffe8
carlos gallegos <gallegoscarlos616@gmail.com>
2023-10-12 11:16:41 -0600

    Cambios

--- Residenciasweb/wwwroot/scripts/login.js
+++ Residenciasweb/wwwroot/scripts/login.js
@@ -1,8 +1,18 @@
 ﻿var formRecuperar = document.querySelector(".formRecuperarContraseña");
 var formMensaje = document.querySelector(".formMensaje");
+var modalElement = document.querySelector(".modal"); 
 
 document.body.addEventListener("click", function (e) {
-    if (e.target.tagName == "A") {
-        formRecuperar.parentElement.style.display = "block";
+    if (e.target.tagName == "A" && e.target.textContent == "¿Olvidaste tu contraseña?") {
+
+        formRecuperar.style.display = "block";
+        formMensaje.style.display = "none";
+        modalElement.classList.add("modal--show");
+    }
+    else if (e.target.tagName == "A" && e.target.textContent == "Registrarte") {
+        formMensaje.style.display = "block";
+        formRecuperar.style.display = "none";
+        modalElement.classList.add("modal--show");
     } 
+    
 });
