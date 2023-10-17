2393beae79b6989fa3dc4f78d1425336eca8b909
carlos gallegos <gallegoscarlos616@gmail.com>
2023-10-17 11:45:52 -0600

    cambios

---
+++ Residenciasweb/wwwroot/scripts/tareas.js
@@ -0,0 +1,35 @@
+﻿//var formtarea1 = document.querySelector(".formtarea1");
+//var formtarea2 = document.querySelector(".formtarea2");
+//var mostrarderecha = document.querySelector(".derecha");
+
+//formtarea2.parentElement.style.display = "none";
+
+//document.body.addEventListener("click", function (e) {
+
+//    if (e.target.tagName == "A" && e.target.textContent =="Tarea 2") {
+
+//        formtarea2.parentElement.style.display = "flex";
+//        formtarea1.parentElement.style.display = "none";
+//    }
+
+//});
+
+
+const tareas = document.querySelectorAll('ul li a');
+
+tareas.forEach(tarea => {
+    tarea.addEventListener('click', () => {
+        const tareaId = tarea.getAttribute('data-tarea');
+        const tareasContainer = document.querySelectorAll('.derecha');
+
+        tareasContainer.forEach(container => {
+            if (container.id === tareaId) {
+                container.style.display = 'flex'; 
+            } else {
+                container.style.display = 'none'; 
+            }
+        });
+    });
+});
+
+
