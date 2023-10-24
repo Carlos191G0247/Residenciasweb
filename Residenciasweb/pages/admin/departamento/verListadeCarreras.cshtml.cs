c923c694808eeda0bcdb626bc8c236b15d156f80
191G0706@rcarbonifera.tecnm.mx <191G0706@rcarbonifera.tecnm.mx>
2023-10-24 16:40:26 -0600

    Cambios

---
+++ Residenciasweb/pages/admin/departamento/verListadeCarreras.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Residenciasweb.pages.admin.departamen
+{
+    public class verListadeCarrerasModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
