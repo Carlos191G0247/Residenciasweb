e8dd3f30e027afe98ba3e6837663e94f5809be66
carlos gallegos <gallegoscarlos616@gmail.com>
2023-10-27 02:55:10 -0600

    Cambios

---
+++ Residenciasweb/pages/login/Solicitudes.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Residenciasweb.pages.login
+{
+    public class SolicitudesModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
