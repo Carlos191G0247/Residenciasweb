2ec2458de91e94d9b5d2b43ae339e96c379653a9
carlos gallegos <gallegoscarlos616@gmail.com>
2024-01-08 15:31:16 -0600

    Cambios

--- Residenciasweb/wwwroot/scripts/login.js
+++ Residenciasweb/wwwroot/scripts/login.js
@@ -84,104 +84,88 @@ document.addEventListener("DOMContentLoaded", function () {
 
    
 
-    //enviarBtn.addEventListener('click', async function (event) {
+    enviarBtn.addEventListener('click', async function (event) {
 
-    //    event.preventDefault();
+        event.preventDefault();
 
-    //    errores = null;
-
-
-
-    //    let form = event.target.closest('form');;
-
-    //    let json = {
-    //        Numcontrol: form.elements.numControl.value,
-    //    };
-
-    //    let response = await fetch("https://apiresidenciaswebca.sistemas19.com/api/CambiarContrasena/login", {
-    //        method: 'POST',
-    //        body: JSON.stringify(json),
-    //        headers: {
-    //            "content-type": "application/json"
-    //        }
-
-    //    });
-
-    //    if (response.ok) {
-    //        let idobj = await response.json();
-    //        console.log(idobj);
-
-
-    //        let json2 = {
-    //            Id: idobj,
-    //        };
-
-    //        let response2 = await fetch("https://apiresidenciaswebca.sistemas19.com/api/CambiarContrasena", {
-    //            method: 'PUT',
-    //            body: JSON.stringify(json2),
-    //            headers: {
-    //                "content-type": "application/json"
-    //            },
-
-    //        });
-
-    //        if (response2.ok) {
-    //            formRecuperar.parentElement.style.display = "none";
-    //            formMensaje.parentElement.style.display = "flex";
-    //            formMensaje2.parentElement.style.display = "none";
-
-    //        }
-    //        else {
-    //            ocultarformulario();
-
-    //        }
+        errores = null;
 
 
 
-    //    }
-    //    else {
-    //        let idobj = await response.text();
-    //        console.log(idobj);
-    //        erroresLabel.textContent = idobj;
+        let form = event.target.closest('form');;
 
-    //    }
+        let json = {
+            Numcontrol: form.elements.numControl.value,
+        };
 
-    //});
+        let response = await fetch("https://apiresidenciaswebca.sistemas19.com/api/CambiarContrasena/login", {
+            method: 'POST',
+            body: JSON.stringify(json),
+            headers: {
+                "content-type": "application/json"
+            }
 
-    //cancelar.addEventListener('click', async function (event) {
-    //    formRecuperar.parentElement.style.display = "none";
-    //    formMensaje.parentElement.style.display = "none";
-    //    formMensaje2.parentElement.style.display = "none";
-    //});
-    //function ocultarformulario() {
-    //    formRecuperar.parentElement.style.display = "none";
-    //    formMensaje.parentElement.style.display = "none";
-    //    formMensaje2.parentElement.style.display = "flex";
-    //}
-    //olvidastecontra.addEventListener('click',function (event) {
-    //    formRecuperar.parentElement.style.display = "flex";
-    //    formMensaje.parentElement.style.display = "none";
-    //    formMensaje2.parentElement.style.display = "none";
-    //});
+        });
+
+        if (response.ok) {
+            let idobj = await response.json();
+            console.log(idobj);
+
+
+            let json2 = {
+                Id: idobj,
+            };
+
+            let response2 = await fetch("https://apiresidenciaswebca.sistemas19.com/api/CambiarContrasena", {
+                method: 'PUT',
+                body: JSON.stringify(json2),
+                headers: {
+                    "content-type": "application/json"
+                },
+
+            });
+
+            if (response2.ok) {
+                formRecuperar.parentElement.style.display = "none";
+                formMensaje.parentElement.style.display = "flex";
+                formMensaje2.parentElement.style.display = "none";
+
+            }
+            else {
+                ocultarformulario();
+
+            }
+
+
+
+        }
+        else {
+            let idobj = await response.text();
+            console.log(idobj);
+            erroresLabel.textContent = idobj;
+
+        }
+
+    });
+
+    cancelar.addEventListener('click', async function (event) {
+        formRecuperar.parentElement.style.display = "none";
+        formMensaje.parentElement.style.display = "none";
+        formMensaje2.parentElement.style.display = "none";
+    });
+    function ocultarformulario() {
+        formRecuperar.parentElement.style.display = "none";
+        formMensaje.parentElement.style.display = "none";
+        formMensaje2.parentElement.style.display = "flex";
+    }
+    olvidastecontra.addEventListener('click',function (event) {
+        formRecuperar.parentElement.style.display = "flex";
+        formMensaje.parentElement.style.display = "none";
+        formMensaje2.parentElement.style.display = "none";
+    });
     
   
-    //document.body.addEventListener("click", function (e) {
-        
-
-    //     if (e.target.tagName == "A" && e.target.textContent == "Registrarte") {
-    //        window.location.href = "/Registro/RegistroAlumno";
-    //    } else if (e.target.tagName === "BUTTON" && e.target.textContent === "Iniciar Sesión") {
-    //        const numeroDeControl = document.getElementById('numeroDeControl').value;
-    //        if (numeroDeControl.length === 4) {
-    //            window.location.href = "/admin/departamento/verListadeCarreras";
-    //        } else if (numeroDeControl.length === 8) {
-    //            window.location.href = "/login/Tareas";
-    //        }
-    //        else if (numeroDeControl.length === 2) {
-    //            window.location.href = "/login/Solicitudes";
-    //        }
-    //    }
-    //});
+   
 });
 
 
